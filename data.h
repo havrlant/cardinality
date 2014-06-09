4373e57cf7742471adbcac125c264ab79ed8c6b8
Lukas Havrlant <lukas@havrlant.cz>
2014-06-09 14:49:04 +0200

    + data from http://goo.gl/iU8Ig

---
+++ data.h
@@ -0,0 +1,16 @@
+//
+//  data.h
+//  cardinality
+//
+//  Created by Lukáš Havrlant on 9.6.14.
+//  Copyright (c) 2014 Lukáš Havrlant. All rights reserved.
+//
+
+#ifndef cardinality_data_h
+#define cardinality_data_h
+
+double raw_estimate_data[15][201];
+double bias_data[15][201];
+int tresholds[15];
+
+#endif
