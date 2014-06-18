0e2d8526ea3e03fcc40cb413857283a9320ab9b6
Lukas Havrlant <lukas@havrlant.cz>
2014-06-18 08:48:18 +0200

    bug fixed

--- hyperloglog.c
+++ hyperloglog.c
@@ -142,7 +142,7 @@ void save_sparse(Hyperloglog *hll, char *filename) {
     uint V = count_zero_buckets(hll);
     if (V >= ((1 / 3.0) * hll->m)) {
         for (uint i = 0; i < hll->m; i++) {
-            if (hll->m > 0) {
+            if (hll->M[i] > 0) {
                 index = i;
                 fwrite(&index, sizeof(uint16_t), 1, fp);
                 fwrite(&(hll->M[i]), sizeof(byte), 1, fp);
