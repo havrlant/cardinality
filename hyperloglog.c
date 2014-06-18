816500d84e9627608daa6c6cff8d1db936484662
Lukas Havrlant <lukas@havrlant.cz>
2014-06-18 10:03:46 +0200

    test

--- hyperloglog.c
+++ hyperloglog.c
@@ -138,26 +138,27 @@ void save_sparse(Hyperloglog *hll, char *filename) {
     safe_path(path, '_');
     
     uint V = count_zero_buckets(hll);
-    uint nonzero = hll->m - V;
-    IndexPair *pairs = (IndexPair*) malloc(nonzero * sizeof(IndexPair));
+    //uint nonzero = hll->m - V;
+    // IndexPair *pairs = (IndexPair*) malloc(nonzero * sizeof(IndexPair));
     uint j = 0;
+    uint16_t index;
     if (V >= ((1 / 3.0) * hll->m)) {
         FILE *fp = fopen(path, "wr");
         
         for (uint i = 0; i < hll->m; i++) {
             if (hll->M[i] > 0) {
-                /*index = i;
-                fwrite(&index, sizeof(uint16_t), 1, fp);
-                fwrite(&(hll->M[i]), sizeof(byte), 1, fp);*/
-                pairs[j].index = (uint16_t) i;
-                pairs[j].value = hll->M[j];
+                index = (uint16_t)i;
+                fwrite(&index, 2, 1, fp);
+                fwrite(&(hll->M[i]), 1, 1, fp);
+                /*pairs[j].index = (uint16_t) i;
+                pairs[j].value = hll->M[j];*/
                 j++;
             }
         }
         
-        fwrite(pairs, sizeof(IndexPair), j, fp);
+        // fwrite(pairs, sizeof(IndexPair), j, fp);
         fclose(fp);
-        printf("Ocekavana velikost: %u B\n", j * (uint)sizeof(IndexPair));
+        printf("Ocekavana velikost: %u B\n", j * 3);
     }
     save_vector(hll, filename);
 }
