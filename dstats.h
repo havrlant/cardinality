f62cf816c1a5722f9cd9c77ebeb70674c19dfc8c
Lukas Havrlant <lukas@havrlant.cz>
2014-06-27 14:45:15 +0200

    + constants for dstats columns

--- dstats.h
+++ dstats.h
@@ -16,9 +16,28 @@
 #define ID_CHANNEL 4
 #define ID_CAMPAIGN 5
 #define ID_PLAN_CAMPAIGN 6
+#define MYSQL_DATE 7
+#define MYSQL_TIME 8
 #define ID_BANNER 9
+#define STATUS 10
+#define UniqueUserIDpartIP 11
+#define BROWSER_ID 12
+#define OS_ID 13
+#define IP_COUNTRY 14
+#define IP_REGION 15
+#define SITE_CATEGORY 16
 #define BANNER_VERSION 17
+#define REMOTE_HOST 18
+#define SCREENWIDTH 19
+#define SCREENHEIGHT 20
+#define COOKIE_ENABLED 21
+#define UniqueUserIDpartUU 22
+#define IP 23
+#define SITE_URL 24
+#define PAGE_KEYWORDS 25
+#define ACTION_TEXT 26
 #define UUID_INDEX 27
+#define MOBILE_DEVICE_ID 28
 
 #include "declarations.h"
 #include <stdlib.h>
